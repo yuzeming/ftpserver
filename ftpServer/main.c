92337781093722e09f7c1d7df8a81b4fd21b5ea3
Zeming YU <yuzeming@gmail.com>
2014-11-10 17:16:15 +0800

    Initial Commit

---
+++ ftpServer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ftpServer
+//
+//  Created by 俞则明 on 14/11/10.
+//  Copyright (c) 2014年 ZemingYU. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
